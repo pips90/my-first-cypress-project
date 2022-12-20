428d6ffff8183b0ebb9b8ee7170f99d0713ffa56
Callie Condray <pippa1090@gmail.com>
2022-12-20 12:38:09 -0600

    add type & click vids to test + timeout in config

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -2,4 +2,9 @@
 
 it("Google Search", () => {
   cy.visit("https://google.com");
+  cy.get(".gLFyf").type("Automation Step by Step{Enter}");
+  // we can use {Enter} instead of what is below
+  //cy.contains("Google Search").click();
+  // add , {timeout:5000} for timeout errors
+  cy.contains("Videos", { timeout: 5000 }).click();
 });
